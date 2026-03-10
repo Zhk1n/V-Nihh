fd3528a4afd71e1efbc9bead5c7e5df2285b23bb
Gitteyyy <davidc.raagas@gmail.com>
2026-03-10 22:20:46 +0800

    created a base

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AddanewTask
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AddanewTask;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnDoneClicked(object sender, EventArgs e)
+    {
+        string taskName = TaskNameEntry.Text;
+
+        if (string.IsNullOrWhiteSpace(taskName))
+        {
+            await DisplayAlertAsync("Error!", "Enter a Task Name", "OK");
+            return;
+        }
+
+        await DisplayAlertAsync("Progress", $"Task '{taskName}' has been added!", "OK");
+
+        TaskNameEntry.Text = string.Empty;
+    }
+}
