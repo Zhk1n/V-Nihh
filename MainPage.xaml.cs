f8e78031f8ad5dcbb90a8fea89862a4db2c2b92d
Raviixi <262235444+Raviixi@users.noreply.github.com>
2026-03-11 09:58:55 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void AboutClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("About Us",
+            "Task App created by the V-Nihh team.",
+            "OK");
+    }
+}
