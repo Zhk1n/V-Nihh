e41fa9cc58a984a6e7c29fbe27484862222eebad
Zhk1n <davidtamayo535@gmail.com>
2026-03-31 10:54:53 +0800

    Completed the UI

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,26 +1,9 @@
-﻿using System;
-
-namespace AddanewTask;
+﻿namespace AddanewTask;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
-    }
-
-    private async void OnDoneClicked(object sender, EventArgs e)
-    {
-        string taskName = TaskNameEntry.Text;
-
-        if (string.IsNullOrWhiteSpace(taskName))
-        {
-            await DisplayAlertAsync("Error!", "Enter a Task Name", "OK");
-            return;
-        }
-
-        await DisplayAlertAsync("Progress", $"Task '{taskName}' has been added!", "OK");
-
-        TaskNameEntry.Text = string.Empty;
     }
 }
