2e046b049cc5d5e10f08afd2bc4c41b254c4c10e
keeonepaullumaoeg-blip <keeonepaullumaoeg@gmail.com>
2026-06-05 10:26:16 +0800

    Add files via upload

---
+++ Pages/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace V_nihh.Pages;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Pages/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace V_nihh.Pages;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
